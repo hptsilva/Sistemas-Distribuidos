112449808a2144b0af66d9a689e31b01792db041
hptsilva <humbertot@hotmail.com.br>
2021-06-02 20:32:12 -0300

    .

--- echo-client.py
+++ echo-client.py
@@ -2,7 +2,9 @@
 
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+localhost = '127.0.0.1'
+
+HOST = localhost  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
